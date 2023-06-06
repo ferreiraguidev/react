37115391a73a596804aa57e06a977fd8e6a67cf3
Guilherme Ferreira <ferreiragui.dev@gmail.com>
2023-06-05 23:09:23 -0300

    no comments

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+
+import { Flex } from '@chakra-ui/react'
+import React from 'react'
+ 
+
+
+export default function Home() {
+
+
+  return (
+
+    <Flex w="100vw" h="100vh" align="center" justifyContent="center">
+      <h3>hello</h3>
+        <Flex as="form" 
+         width="100%" 
+         maxWidth={360}
+         backgroundColor="gray.800"
+         border-radius="8">
+         </Flex>
+    </Flex>
+
+  )
+}
---
+++ src/pages/user/index.tsx
@@ -0,0 +1,15 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+
+
+
+export default function Rice(){
+    return (
+
+        <Flex>
+            <h2>AKSDOPKAOPSDKA</h2>
+        </Flex>
+        
+
+    );
+}
